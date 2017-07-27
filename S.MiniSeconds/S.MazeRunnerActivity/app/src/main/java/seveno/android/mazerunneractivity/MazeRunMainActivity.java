25b0fcbb2cf5d6c5eda96a83c1bba292c29ddde8
kpn <kpn@com>
2017-07-28 02:13:37 +0900

    sds

---
+++ S.MiniSeconds/S.MazeRunnerActivity/app/src/main/java/seveno/android/mazerunneractivity/MazeRunMainActivity.java
@@ -0,0 +1,13 @@
+package seveno.android.mazerunneractivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MazeRunMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maze_run_main);
+    }
+}
---
+++ S.MiniSeconds/S.MazeRunnerActivity/app/src/main/java/seveno/android/mazerunneractivity/MazeRunnerClass/Ball.java
@@ -0,0 +1,12 @@
+package seveno.android.mazerunneractivity.MazeRunnerClass;
+import android.os.SystemClock;
+
+import java.util.Timer;
+import java.util.TimerTask;
+/**
+ * Created by 김태훈 on 2017-07-28.
+ */
+
+public class Ball {
+
+}
