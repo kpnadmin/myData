5c5015c41f985277c323a4bd15888489fe90d276
kpn <kpn@com>
2017-08-01 01:28:43 +0900

    sdfdsfdsf

--- S.MiniSeconds/S.MazeRunnerActivity/app/src/main/java/seveno/android/mazerunneractivity/MazeRunMainActivity.java
+++ S.MiniSeconds/S.MazeRunnerActivity/app/src/main/java/seveno/android/mazerunneractivity/MazeRunMainActivity.java
@@ -9,5 +9,8 @@ public class MazeRunMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maze_run_main);
+
+
+
     }
 }
