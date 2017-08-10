343b7cb8dc797658c342f85764488411b22c39dc
kpn <kpn@naver.com>
2017-08-10 18:52:07 +0900

    asd

---
+++ MiniSeconds/app/src/main/java/seveno/android/miniseconds/AvoidStarGame/Cust.java
@@ -0,0 +1,11 @@
+package seveno.android.miniseconds.AvoidStarGame;
+
+/**
+ * Created by Administrator on 2017-08-10.
+ */
+
+public class Cust {
+
+    
+
+}
---
+++ MiniSeconds/app/src/main/java/seveno/android/miniseconds/AvoidStarGame/StarView.java
@@ -0,0 +1,18 @@
+package seveno.android.miniseconds.AvoidStarGame;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017-08-10.
+ */
+
+public class StarView extends View{
+
+
+    public StarView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
